0a05b3b438874c42791d9c0f7cf7611a04939717
Alexander Olsen <aolsenjazz@gmail.com>
2025-01-20 10:10:27 -0500

    fix: left lots of files uncommmitted

--- plugin/Straudio/Straudio.cpp
+++ plugin/Straudio/Straudio.cpp
@@ -1,23 +1,31 @@
 #include "Straudio.h"
 #include "IPlug_include_in_plug_src.h"
 #include "IPlugPaths.h"
+#include <windows.h>
+#include <stdio.h>
 
 Straudio::Straudio(const InstanceInfo& info)
 : Plugin(info, MakeConfig(kNumParams, kNumPresets))
 {
+    AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
+    printf("Console attached!\n");
   GetParam(kGain)->InitGain("Gain", -70., -70, 0.);
   
 //#ifdef DEBUG
   SetCustomUrlScheme("iplug2");
   SetEnableDevTools(true);
 //#endif
-  
+  printf("3");
   mEditorInitFunc = [&]() {
 //    LoadURL("http://localhost:5173/");
+      printf("1");
     LoadIndexHtml(__FILE__, GetBundleID());
+    printf(__FILE__);
     EnableScroll(false);
   };
-  
+  printf("2");
   MakePreset("One", -70.);
   MakePreset("Two", -30.);
   MakePreset("Three", 0.);
@@ -48,6 +56,8 @@ void Straudio::OnReset()
 void Straudio::OnIdle()
 {
 //  mSender.TransmitData(*this);
-  
-//  Straudio::SendArbitraryMsgFromDelegate(0, strlen(mMessage) + 1, (void*)mMessage);
+    //const char* mMessage = __FILE__;
+    const char* mMessage = GetBundleID();
+    Straudio::SendArbitraryMsgFromDelegate(0, strlen(mMessage) + 1, (void*)mMessage);
+    
 }
